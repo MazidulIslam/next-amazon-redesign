5189c652218ad7643d8905996a6069fb9a989e15
MazidulIslam <mazidulislammym@gmail.com>
2023-02-18 00:13:02 +0600

    axios,api [id],convertDocToObj,updatecart check

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,55 @@
 import ProductItem from '@/components/ProductItem';
 import data from '@/utils/data';
 import Layout from '@/components/Layout';
+import db from '@/utils/db';
+import Product from '@/models/Product';
+import { useContext } from 'react';
+import { CART_ADD_ITEM, Store } from '@/utils/store';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
-export default function Home() {
+export default function Home({ products }) {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+
+  const addToCartHandler = async (product) => {
+    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock < quantity) {
+      // if (product.countInStock < quantity) {
+      alert('Sorry. Product is out of stock');
+      return;
+    }
+    dispatch({ type: CART_ADD_ITEM, payload: { ...product, quantity } });
+    // router.push('/cart');
+    toast.success('Product added to the cart');
+  };
+
   return (
     <Layout title="Home Page">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {data.products?.map((product) => {
+        {products?.map((product) => {
           return (
-            <ProductItem product={product} key={product.slug}></ProductItem>
+            <ProductItem
+              addToCartHandler={addToCartHandler}
+              product={product}
+              key={product.slug}
+            ></ProductItem>
           );
         })}
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find().lean();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
